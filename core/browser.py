41aa233bd082bf84ccadfe7c1610b67303a119be
Sidd <tahasidd33@gmail.com>
2026-06-03 18:25:39 +0530

    Added persistent launch context

--- core/browser.py
+++ core/browser.py
@@ -3,8 +3,13 @@ from playwright.sync_api import sync_playwright
 class Browser:
     def __init__(self):
         self.p = sync_playwright().start()
-        self.browser = self.p.chromium.launch(headless=False)
-        self.page = self.browser.new_page()
+
+        self.context = self.p.chromium.launch_persistent_context(
+            user_data_dir="profile",
+            headless=False
+        )
+
+        self.page = self.context.pages[0] if self.context.pages else self.context.new_page()
         
     def open(self,url):
         self.page.goto(url)
@@ -14,5 +19,5 @@ class Browser:
         self.page.wait_for_timeout(ms)
             
     def close(self):
-        self.browser.close()
+        self.context.close()
         self.p.stop()
